e3cf6fb6b93ed769e61d9e815304cd1eadedc4e8
Ahmad Barakat <77456753+lorddrake4444@users.noreply.github.com>
2021-08-30 19:22:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const dbURI = 'mongodb+srv://Ahmad:1234@cluster0.zuwe4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const express = require ('express');
 const mongoose = require('mongoose');
 const axios = require("axios").default;
 const blogroute = require('./routes/blogroutes');
 const app = express();
-const dbURI = 'mongodb+srv://Ahmad:1234@cluster0.zuwe4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 
 mongoose.connect(dbURI, {useNewUrlParser: true , useUnifiedTopology: true})
 .then((result)=>app.listen(process.env.PORT||3000))
@@ -44,8 +46,14 @@ app.post('/data',(req,res)=>
     }
   };
   axios.request(options).then(function (response) {
-    res.render('data',{country:response.data[0].country , confirmed:response.data[0].confirmed , recovered: response.data[0].recovered
-      ,deaths:response.data[0].deaths ,title:'data'});
+    if(response.data.length!=0){
+      res.render('data',{country:response.data[0].country , confirmed:response.data[0].confirmed , recovered: response.data[0].recovered
+        ,deaths:response.data[0].deaths ,title:'data'});
+    }
+    else
+    {
+      res.render('data',{country:'select country' ,confirmed: 'none' , recovered: 'none', deaths: 'none',title:'data'});
+    }
   }).catch(function (error) {
     console.error(error);
   });
