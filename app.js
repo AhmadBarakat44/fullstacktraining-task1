d092230e140c740a91405fe2a346bdee3a1d7105
Ahmad Barakat <77456753+lorddrake4444@users.noreply.github.com>
2021-08-30 18:07:26 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const dbURI = 'mongodb+srv://Ahmad:1234@cluster0.zuwe4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, {useNewUrlParser: true , useUnifiedTopology: true})
-.then((result)=>app.listen(3000))
+.then((result)=>app.listen(process.env.PORT||3000))
 .catch((err)=>console.log(err));
 
 app.set('view engine' , 'ejs');
